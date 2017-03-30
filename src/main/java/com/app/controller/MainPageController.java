abdc4a128da13d4d85436bb5e69832c854adc032
于景洋 <yujingyang@MacBook-Pro.local>
2017-03-30 20:30:43 +0800

    add git_ignore and readme

---
+++ src/main/java/com/app/controller/MainPageController.java
@@ -0,0 +1,14 @@
+package com.app.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by yujingyang on 2017/3/30.
+ */
+@Controller
+@RequestMapping(value = "main")
+public class MainPageController {
+
+
+}
